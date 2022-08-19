4f66e98115c8bc71c0d0e8cc69871be6106fd6c3
Dillon Ellis <dellis@fcul.com>
2022-08-19 08:58:24 -0400

    original oppurtunites page

---
+++ oppurtunities.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+<html lang="en">
+<head>
+<title>LSCU | Advertising/Sponsorship Opportunities</title>
+<meta charset="utf-8">
+<!-- Meta -->
+<meta name="keywords" content="" />
+<meta name="author" content="">
+<meta name="robots" content="" />
+<meta name="description" content="" />
+
+<!-- this styles only adds some repairs on idevices  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="../images/favicon.ico">
+
+<!-- Google fonts - witch you want to use - (rest you can just remove) -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- stylesheets -->
+<link rel="stylesheet" media="screen" href="../js/bootstrap/bootstrap.min.css" type="text/css" />
+<link rel="stylesheet" href="../js/megamenu/stylesheets/screen.css" type="text/css" />
+<link rel="stylesheet" href="../css/default.css" type="text/css" />
+<link rel="stylesheet" href="../css/layouts.css" type="text/css" />
+<link rel="stylesheet" href="../css/shortcodes.css" type="text/css" />
+<link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" media="screen" href="../css/responsive-leyouts.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
+<link rel="stylesheet" href="../js/tabs/assets/css/responsive-tabs.css" />
+<link rel="stylesheet" type="text/css" href="../js/tabs/assets/css/responsive-tabs.css">
+<link rel="stylesheet" href="../css/et-line-font/et-line-font.css">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-12265204-1');
+</script>
+	
+	<style>
+	.myButton {
+	background-color:#afd460;
+	border-radius:9px;
+	border:1px solid #afd460;
+	display:inline-block;
+	cursor:pointer;
+	color:#000000;
+	font-family:Arial;
+	font-size:17px;
+	padding:17px 31px;
+	text-decoration:none;
+}
+.myButton:hover {
+	background-color:#afd460;
+	color:#fff;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+	</style>
+
+</head>
+
+<body>
+<div class="site_wrapper">
+  <div class="topbar light topbar-padding">
+    <?php include '../inc/topbar.php';?>
+  </div>
+  <div class="clearfix"></div>
+  
+  <div id="header" class="header-section style1 pin-style">
+    <?php include '../inc/nav.php';?>
+  </div>
+  <!--end menu-->
+  <div class="clearfix"></div>
+  
+  <section>
+    <div class="header-inner two">
+      <div class="inner text-center">
+        <h4 class="titleNEW uppercase">Advertising/Sponsorship Opportunities</h4>
+      </div>
+      <img src="../images/headerBG.png" alt="" class="img-responsive"/> </div>
+  </section>
+  <!-- end header inner -->
+  <div class="clearfix"></div>
+  
+ <section class="sec-padding">
+    <div class="container">
+      <div class="row">
+		  <div class="col-md-4 col-sm-12 col-xs-12 bmargin">
+          <div class="col-md-12 col-sm-12 col-xs-12 nopadding bmargin">
+            <ul class="category-links">
+              <li><a href="about-cus.php">About Credit Unions</a></li>
+              <li><a href="opportunities.php">Advertising/Sponsorship Opportunities</a></li>
+              <li><a href="membership.php">LSCU Membership</a></li>
+              
+			  <li><a href="pvt/2022-LSCU-RESOURCE-GUIDE.pdf" target="_blank">LSCU Resource Guide & Credit Union Directory</a></li>
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+		<br>
+		  <div class="col-md-12 col-sm-12 col-xs-12 nopadding bmargin">
+            <div class="feature-box7 text-center active bmargin"> <span class="icon-chat"></span><br/>
+            <br/>
+            <h4 class="uppercase">How Can We Help?</h4>
+            <p>Questions about membership or how to connect with colleagues? We have answers!</p>
+            <br/>
+            <a class="btn btn-border light btn-round" href="mailto:memberengagement@lscu.coop">Contact Us Now</a></div>
+			  
+          </div>
+          <div class="clearfix"></div>
+          <!--end sidebar box-->
+          
+        </div>
+        <!--end left column-->
+        
+        <div class="col-md-8 col-sm-12 col-xs-12 bmargin">
+			<p>The League of Southeastern Credit Unions & Affiliates (LSCU) is one of the premier trade associations for credit unions in the country. It provides products and services for credit unions in Alabama, Florida and Georgia. It also offers exceptional value for your business to connect with credit unions through a variety of advertising and sponsorship opportunities.</p>
+			<p>By advertising or sponsoring a LSCU event or publication, you will be putting your organization in front of credit union leaders and decision makers. LSCU is a trusted partner to many credit unions who look to its best-in-class services and solutions. Advertising or sponsoring with the LSCU demonstrates your alignment with an organization that is solely focused on the success of credit unions and the millions of members it serves. The advertising and sponsorship opportunities are customizable and can fit any marketing budget.</p>
+			
+			<h3><strong>LSCU Ad/Event Sponsorship Opportunities</strong><br><br>
+			<div style="position:relative;padding-top:max(60%,326px);height:0;width:100%"><iframe title="LSCU Ad" sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" allowfullscreen="true" style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;" src="https://e.issuu.com/embed.html?d=2022-lscu-sponsorship-kit-021022&u=lscu"></iframe></div></h3>
+			
+			<p align="center"><a href="https://lscu.coop/about/sponsorship-order-form.php" class="myButton">Become a LSCU Sponsor Today!</a></p>
+		  
+		  <!--<p>Become an LSCU sponsor today through the <a href="https://lscu.coop/about/sponsorship-order-form.php">2022 Sponsorship Kit</a>! By sponsoring an LSCU event, you will be putting your organization in front of credit union leaders and decision makers.</p>-->
+		  
+		  <!--<p>For a PDF copy of the Sponsorship Kit, please <a href="docs/2022-LSCU-Sponsorship-Kit.pdf" target="_blank">click here</a>.</p>-->
+		  
+		  <h3 id="media"><strong>LSCU Media Kit</strong><br><br>
+		  <div style="position:relative;padding-top:max(60%,326px);height:0;width:100%"><iframe title="LSCU Media Kit" sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" allowfullscreen="true" style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;" src="https://e.issuu.com/embed.html?d=2022-media-kit&u=lscu"></iframe></div></h3>
+			
+			<p align="center"><a href="https://lscu.coop/about/media-kit-form.php" class="myButton">Advertise with LSCU Today!</a></p>
+		
+		<!--<p>Advertise with LSCU today through the <a href="https://lscu.coop/about/media-kit-form.php">2022 Media Kit</a>! By advertising with LSCU, you can reach hundreds of credit unions and credit union professionals looking for best-in-class services and solutions.</p>-->
+			
+			
+			
+		  <p>&nbsp;</p>
+			
+			
+			<div class="col-md-12 col-sm-12 col-xs-12 bmargin" align="center">
+			<div class="box" style="width: 50%;">							
+				<div class="icon">
+					<div class="image"><i class="fa fa-link"></i></div>
+					<div class="info">
+						<h3 class="title">Request More Information</h3>
+
+						<div class="more">
+							<a href="mailto:education@lscu.coop">
+								Request Now <i class="fa fa-angle-double-right"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="space"></div>
+			</div>
+			</div>
+			
+        </div>
+        
+      </div>
+    </div>
+  </section>
+  <!-- end section -->
+  <div class="clearfix"></div>
+  
+  <section class="section-copyrights sec-moreless-padding">
+	<?php include '../inc/footer.php';?>
+  </section>
+  <!--end section-->
+  <div class="clearfix"></div>
+  
+  <a href="#" class="scrollup blue"><span style="display:none;">Scroll To Top Button</span></a><!-- end scroll to top of the page--> 
+  
+</div>
+<!--end sitewraper--> 
+
+<!-- ========== Js files ========== --> 
+
+<script type="text/javascript" src="../js/universal/jquery.js"></script> 
+<script src="../js/bootstrap/bootstrap.min.js" type="text/javascript"></script> 
+<script src="../js/megamenu/js/main.js"></script> 
+<script src="../js/mainmenu/jquery.sticky.js"></script> 
+<script src="../js/scrolltotop/totop.js"></script> 
+<script type="text/javascript" src="../js/tabs/smk-accordion.js"></script>
+<script type="text/javascript" src="../js/tabs/custom.js"></script>
+<script src="../js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+<script src="../js/scripts/functions.js" type="text/javascript"></script>
+</body>
+</html>
